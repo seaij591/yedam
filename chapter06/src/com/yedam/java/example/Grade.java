c3d786839d6459868dffcfc6eeae8e789dc24851
admin <admin@YD5-19>
2022-01-04 17:15:15 +0900

    01.04

--- chapter06/src/com/yedam/java/example/Grade.java
+++ chapter06/src/com/yedam/java/example/Grade.java
@@ -1,18 +1,24 @@
 package com.yedam.java.example;
 
 public class Grade {
-
+	//필드
 	int math;
 	int science;
 	int english;
-
+	
+	//생성자
 	Grade(int math, int science, int english) {
 		this.math = math;
 		this.science = science;
 		this.english = english;
 	}
-
+	
+	//메서드
 	double average() {
+//		double avg =0;
+//		int sum = math + science + english;
+//		avg = sum /3.0;
+//		return avg;
 		return (this.math + this.science + this.english) / 3.0;
 	}
 }
--- chapter06/src/com/yedam/java/example/TV.java
+++ chapter06/src/com/yedam/java/example/TV.java
@@ -1,19 +1,21 @@
 package com.yedam.java.example;
 
 public class TV {
-
+	//필드
 	String company ;
 	int scine ;
 	int size ;
 	
-	
+	//생성자
 	TV(String company, int scine, int size){
 		this.company = company;
 		this.scine = scine;
 		this.size = size;
 		
 	}
+	//메서드
 	void showInfo() {
-		System.out.println(company+"에서 만든"+scine+"년형"+size+"인치 TV");
+		System.out.println(company+"에서 만든"+scine
+						+"년형"+size+"인치 TV");
 	}
 }
